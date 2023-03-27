0634b6796c4c00d22bcb4dd5268d47276e60b6be
odey abdalrahman <odey.abdalrahman@dal.shj.ae>
2023-03-27 20:46:18 +0400

    Frist Commit

---
+++ src/Extensions/StartDiagnosticExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Distributed.Tracing.Extensions
+{
+    public static class StartDiagnosticExtension
+    {
+        public static void StartActivity(string listener, string name, object? tags)
+        {
+            var source = new DiagnosticListener(listener);
+            var activity = new Activity(name);
+            using var act = source.StartActivity(activity, tags);
+        }
+    }
+}
---
+++ src/ViewModel/ZipkinViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Distributed.Tracing;
+
+public class ZipkinViewModel
+{
+    public string? ZipkinAddress { get; set; }
+}
